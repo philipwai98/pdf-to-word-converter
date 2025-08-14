7cb4402fd9af784d7b0041c5ae065ece18aca035
philipwai98 <philipwai.training@gmail.com>
2025-08-14 09:12:12 +0800

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask, request, render_template, send_file
-from pdf2docx import Converter
-import os, uuid
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        file = request.files['pdf_file']
-        if file:
-            pdf_name = f"temp_{uuid.uuid4()}.pdf"
-            docx_name = f"temp_{uuid.uuid4()}.docx"
-
-            file.save(pdf_name)
-            cv = Converter(pdf_name)
-            cv.convert(docx_name, start=0, end=None)
-            cv.close()
-
-            return send_file(docx_name, as_attachment=True)
-
+@app.route('/')
+def home():
     return render_template('index.html')
 
+@app.route('/merge')
+def merge():
+    return render_template('merge.html')
+
+@app.route('/split')
+def split():
+    return render_template('split.html')
+
+@app.route('/compress')
+def compress():
+    return render_template('compress.html')
+
+@app.route('/password')
+def password():
+    return render_template('password.html')
+
+@app.route('/pdf2word')
+def pdf2word():
+    return render_template('pdf2word.html')
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
